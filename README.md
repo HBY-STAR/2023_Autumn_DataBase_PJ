6c7c61b42cd38f8a5f756bd8d5c1889d0e09eb5a
ppolariss <2673986789@qq.com>
2023-12-04 19:22:31 +0800

    feat: readme

--- README.md
+++ README.md
@@ -6,12 +6,18 @@
 
 线上购物比价系统：
 
-1. GenerateData用于生成测试用的数据，使用java编写。
+1. GenerateData用于生成数据，使用java编写。
 2. client为前端，使用vue编写。
 3. src为后端，使用go编写。
 
 ## 使用说明
 
+### 数据生成和导入
 1. GenerateData：运行RunAndGenerate.java即可生成json格式的测试数据
-2. client：在client目录下，命令行执行npm run dev即可启动前端，部署在localhost:8070。
-3. src：在src/models/init.go中配置数据库。src/utils下运行go test json将测试数据导入数据库。src下运行go build main.go启动后端，部署在127.0.0.1:8080。
+2. 创建数据库表price_comparator（或使用price_comparator.sql导入）
+3. 在src/models/init.go 中配置mysql
+4. src/utils/json 运行 `go test` 生成数据库表并导入GenerateData中的数据
+
+### 构建和运行
+1. client：在client目录下，命令行执行npm run dev即可启动前端，部署在localhost:8070。
+2. src：运行go build main.go启动后端，部署在127.0.0.1:8080。
---
+++ price_comparator.sql
@@ -0,0 +1,167 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 8.0.32 : Database - price_comparator
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`price_comparator` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `price_comparator`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `commodity` */
+
+DROP TABLE IF EXISTS `commodity`;
+
+CREATE TABLE `commodity` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `default_name` varchar(64) NOT NULL,
+  `produce_at` datetime(3) NOT NULL,
+  `produce_address` varchar(64) NOT NULL,
+  `category` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `commodity_item` */
+
+DROP TABLE IF EXISTS `commodity_item`;
+
+CREATE TABLE `commodity_item` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `commodity_id` bigint NOT NULL,
+  `platform_id` bigint NOT NULL,
+  `seller_id` bigint NOT NULL,
+  `item_name` varchar(64) NOT NULL,
+  `price` float NOT NULL,
+  `update_at` datetime(3) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_commodity_item_commodity` (`commodity_id`),
+  KEY `fk_commodity_item_platform` (`platform_id`),
+  KEY `fk_commodity_item_seller` (`seller_id`),
+  CONSTRAINT `fk_commodity_item_commodity` FOREIGN KEY (`commodity_id`) REFERENCES `commodity` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_commodity_item_platform` FOREIGN KEY (`platform_id`) REFERENCES `platform` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_commodity_item_seller` FOREIGN KEY (`seller_id`) REFERENCES `seller` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `chk_commodity_item_price` CHECK ((`price` > 0))
+) ENGINE=InnoDB AUTO_INCREMENT=2001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `favorite` */
+
+DROP TABLE IF EXISTS `favorite`;
+
+CREATE TABLE `favorite` (
+  `user_id` bigint NOT NULL,
+  `commodity_item_id` bigint NOT NULL,
+  `price_limit` float NOT NULL DEFAULT '0',
+  `update_at` datetime(3) DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`commodity_item_id`),
+  KEY `fk_favorite_commodity_item` (`commodity_item_id`),
+  CONSTRAINT `fk_favorite_commodity_item` FOREIGN KEY (`commodity_item_id`) REFERENCES `commodity_item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_favorite_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `message` */
+
+DROP TABLE IF EXISTS `message`;
+
+CREATE TABLE `message` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `commodity_item_id` bigint DEFAULT NULL,
+  `current_price` float NOT NULL,
+  `create_at` datetime(3) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_message_commodity_item` (`commodity_item_id`),
+  KEY `fk_message_user` (`user_id`),
+  CONSTRAINT `fk_message_commodity_item` FOREIGN KEY (`commodity_item_id`) REFERENCES `commodity_item` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_message_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `platform` */
+
+DROP TABLE IF EXISTS `platform`;
+
+CREATE TABLE `platform` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `url` varchar(64) DEFAULT NULL,
+  `country` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `price_change` */
+
+DROP TABLE IF EXISTS `price_change`;
+
+CREATE TABLE `price_change` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `commodity_item_id` bigint NOT NULL,
+  `new_price` float NOT NULL,
+  `update_at` datetime(3) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_price_change_commodity_item` (`commodity_item_id`),
+  CONSTRAINT `fk_price_change_commodity_item` FOREIGN KEY (`commodity_item_id`) REFERENCES `commodity_item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=501 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `seller` */
+
+DROP TABLE IF EXISTS `seller`;
+
+CREATE TABLE `seller` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `address` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `age` tinyint NOT NULL,
+  `gender` tinyint(1) NOT NULL,
+  `phone` char(13) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `user_jwt_secret` */
+
+DROP TABLE IF EXISTS `user_jwt_secret`;
+
+CREATE TABLE `user_jwt_secret` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `secret` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
